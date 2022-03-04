5e3d4b67be542bb91708145de93af45b4fe4dfb2
Pixselve <38262536+Pixselve@users.noreply.github.com>
2022-03-04 18:24:04 +0100

    chore: base files for TP2

---
+++ Liste/copier.h
@@ -0,0 +1,16 @@
+/**
+   3.2.1 chercher une valeur dans une liste triée
+   @param l : liste où se fait la recherche
+   @param x : valeur à chercher
+   @return position du premier élément de valeur >= x
+   ou end() si un tel élément n'existe pas
+ */
+// TODO
+
+
+/**
+   3.2.2 créer une copie triée par valeurs croissantes d'une liste
+   @param l : liste à copier
+   @return liste triée
+*/
+// TODO
